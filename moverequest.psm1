ea7f05a4c5e115b84fc5d8dc08ae83d0e6d2c588
seboo30 <wild.sebastian@outlook.de>
2021-04-06 17:26:37 +0200

    progress bar

--- moverequest.psm1
+++ moverequest.psm1
@@ -43,7 +43,7 @@ function New-O365MoveRequest {
             $s = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $cred -Authentication Basic -AllowRedirection
             $importresults = Import-PSSession $s -AllowClobber
 
-            $moverequests = Get-Moverequest 
+
         }
         catch {
             Throw "Could not connect to Office 365"
@@ -54,44 +54,28 @@ function New-O365MoveRequest {
     process {
         try {
             $endpoint = Get-MigrationEndPoint
+            $mailboxes = Get-Mailbox
+            $moverequests = Get-Moverequest 
         }
         catch {
-            throw "No Migrationendpoint found !"
+            throw "Couldnt get relevant Information, e.g Mailboxes / MoveRequests or the Hybrid Migrationendpoint !"
             $_
         }
 
         if ($Group) {
             $users = Get-ADGroupMember -Identity $group | Where-Object ( { $_.Name -notin $namefilter }) | ForEach-Object { (Get-ADUser $_.SamAccountName -Properties * | where-object { $null -ne $_.msExchRecipientTypeDetails }) } | Select-Object Name, userPrincipalName
-
-            Foreach ($user in $users) {
-
-                try {
-                    Foreach ($r in $moverequests) {
-
-                        if ($r.DisplayName -ne $user.Name) {
-	
-                            New-MoveRequest -Erroraction Stop -Identity $user.userPrincipalName -Remote -RemoteHostName $endpoint.RemoteServer -TargetDeliveryDomain $TargetDeliveryDomain -RemoteCredential $opcred -SuspendWhenReadyToComplete:$($Suspendwhenreadytocomplete) | Out-Null
-                            Write-Host 'MoveRequest für ' $user.Name ' erstellt' -ForeGroundColor Green
-                        }
-                    }
-                }
-                catch {
-                    Write-Host 'Fehler bei '$user.Name -ForeGroundColor Red
-                    Write-Host $_
-                }
-            }
         }
         else {
             $users = Get-ADUser -SearchBase $ou -Properties * | Where-Object { $_.SamAccountName -notin $namefilter } -and { $null -ne $_.msExchRecipientTypeDetails } | Select-Object Name, UserPrincipalName
-
+        }
+        $i = 0
+        Foreach ($user in $users) {
+            $i = $i + 1
+            Write-Progress -Activity "Generating MoveRequests" -Id 1 -Status "Processing $i/$($users.count) User" -PercentComplete ($i / $users.count * 100)
             try {
-                Foreach ($r in $moverequests) {
-
-                    if ($r.DisplayName -ne $user.Name) {
-
-                        New-MoveRequest -Erroraction Stop -Identity $user.userPrincipalName -Remote -RemoteHostName $endpoint.RemoteServer -TargetDeliveryDomain $TargetDeliveryDomain -RemoteCredential $opcred -SuspendWhenReadyToComplete:$($Suspendwhenreadytocomplete) | Out-Null
-                        Write-Host 'MoveRequest für ' $user.Name ' erstellt' -ForeGroundColor Green
-                    }
+                if (($user.Name -notin $moverequests.DisplayName) -or ($user.Name -notin $mailboxes.Name)) {
+                    New-MoveRequest -Erroraction Stop -Identity $user.userPrincipalName -Remote -RemoteHostName $endpoint.RemoteServer -TargetDeliveryDomain $TargetDeliveryDomain -RemoteCredential $opcred -SuspendWhenReadyToComplete:$($Suspendwhenreadytocomplete) | Out-Null
+                    Write-Host 'MoveRequest für' $user.Name' erstellt' -ForeGroundColor Green
                 }
             }
             catch {
@@ -99,6 +83,7 @@ function New-O365MoveRequest {
                 Write-Host $_
             }
         }
+        
     }
     end {
         Get-PSSession | Remove-PSSession
@@ -165,8 +150,12 @@ function Set-O365MailboxSettings {
     }
     
     process {
-        foreach ($mailbox in (Get-Mailbox)) {
-            Set-MailboxRegionalConfiguration -Identity $mailbox.UserPrincipalName -LocalizeDefaultFolderName:$true -Language De-de -DateFormat "dd.MM.yyyy"
+        $mailboxes = Get-Mailbox
+        $i = 0
+        foreach ($mailbox in $mailboxes) {
+            $i = $i + 1
+            Write-Progress -Activity "Adding Regionalsettings on Mailboxes" -Id 1 -Status "Processing $i/$($mailboxes.count) mailboxes" -PercentComplete ($i / $mailboxes.count * 100)
+            Set-MailboxRegionalConfiguration -Identity $mailbox.UserPrincipalName -Language De-de -DateFormat "dd.MM.yyyy" -TimeZone "W. Europe Standard Time" | Out-Null
         }
     }
     
--- new-profile.ps1
+++ new-profile.ps1
@@ -1,8 +1,26 @@
 if (!(Test-Path $profile)) { 
-    New-Item -ItemType File -Path $PROFILE -Force
-    if (!(Test-Path $HOME\Documents\PowerShell\Modules )) {
-        New-Item -Type Directory -Path $HOME\Documents\PowerShell\Modules
-        $Env:PSModulePath = $Env:PSModulePath + ";$($HOME)\Documents\PowerShell\Modules" | Add-Content -Path $profile 
-        Copy-Item .\DynaPowershell -Destination $HOME\Documents\PowerShell\Modules\ -Recurse
+    try {
+        New-Item -ItemType File -Path $PROFILE -Force
+        if (!(Test-Path $HOME\Documents\PowerShell\Modules )) {
+            New-Item -Type Directory -Path $HOME\Documents\PowerShell\Modules -Force
+            '$Env:PSModulePath = $Env:PSModulePath + ";$($HOME)\Documents\PowerShell\Modules"' | Add-Content $profile
+            New-Item -Path "$($HOME)\Documents\PowerShell\Modules" -ItemType Directory -Name DynaPowershell -Force
+        }
+    }
+    catch {
+        Write-Host $_
     }
 }
+
+try {
+    Invoke-WebRequest -Method Get -Uri "https://github.com/seboo30/Productive/archive/refs/heads/main.zip" -OutFile .\main.zip
+    if (!(Test-Path "$($HOME)\Documents\PowerShell\Modules\DynaPowershell")) {
+        New-Item -Path "$($HOME)\Documents\PowerShell\Modules" -ItemType Directory -Name DynaPowershell -Force
+        Expand-Archive .\main.zip -DestinationPath "$($HOME)\Documents\PowerShell\Modules\DynaPowershell" 
+        Remove-Item .\main.zip -Force
+    }
+}
+catch {
+    Write-Host $_
+}
+
